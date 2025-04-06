f151fe0adf0739412131bb59780582cc57955c83
Lucas Fernandes <68709936+LucasFernandes16@users.noreply.github.com>
2025-04-06 13:51:04 -0300

    Escondendo a chave da api

--- back-fitplanner/montar_treino.py
+++ back-fitplanner/montar_treino.py
@@ -3,10 +3,10 @@ from classes import *
 import openai
 
 def receber_resposta(user: usuario) -> str:
-    openai.api_key = ""
-                        
+    openai.api_key = "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"
+
     prompt: str = criar_prompt(user)
-    resposta = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+    resposta = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = [{"role": "user", "content": prompt}])
     
     return resposta["choices"][0]["message"]["content"]
 
@@ -21,22 +21,24 @@ def montar_treino(user: usuario) -> treino:
         "sabado": []
     }
     
-    # Mapeia os índices de dias disponíveis para os nomes dos dias
-    dias_semana = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"]
-    dias_disponiveis = [dias_semana[i] for i in user.dias_disponiveis]
-    
     lista_treinos: list[list[str]] = receber_resposta(user).split("#")
+    
+    dias_treino: list[int] = user.dias_disponiveis.copy()
+    chaves_cronograma = list(cronograma.keys())
     
     for treino in lista_treinos:
         try:
-            cronograma[dias_disponiveis[0]] = eval(treino)
+            cronograma[chaves_cronograma[dias_treino[0]]] = eval(treino)
         except:
-            cronograma[dias_disponiveis[0]] = [treino.strip()]
+            cronograma[chaves_cronograma[dias_treino[0]]] = [treino.strip()]
+
+    
         
-        dias_disponiveis.pop(0)
+        dias_treino.pop(0)
         
     for dia in cronograma.keys():
         if not cronograma[dia]:
             cronograma[dia] = ["descanso"]
 
+            
     return cronograma
